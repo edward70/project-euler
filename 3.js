f8a0d76661dda9932f9057b4f35fe33246a2a0e2
Edward <76045667+edward70@users.noreply.github.com>
2021-04-10 22:20:33 +1000

    add better method

--- 3.js
+++ 3.js
@@ -1,4 +1,4 @@
-// tried to do it the smart way but the dumb way made more sense
+// simple method
 let num = 600851475143;
 for (let i = 2; i < num; i++) {
     if (num % i === 0) {
@@ -6,3 +6,16 @@ for (let i = 2; i < num; i++) {
     }
 }
 console.log(num);
+
+// better method
+function largestPrimeFactor(num) {
+  for (let i = 2; i < Math.sqrt(num); i++) { // in a composite number a prime factor can't be larger than the sqrt
+    if (num % i === 0) { // check if it's a factor
+      let lpf = largestPrimeFactor(i); // largest prime factor of factor
+      let colpf = largestPrimeFactor(num/i); // largest prime factor of corresponding factor
+      return lpf > colpf ? lpf : colpf; // return the bigger one
+    }
+  }
+  return num; // no matches so it's a prime
+}
+console.log(largestPrimeFactor(600851475143));
